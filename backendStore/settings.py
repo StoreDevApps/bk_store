111614f8b6832ce97f4ad857c8628ae1bc59ffef
Jorge Aaron Zambrano Choez <joraazam@espol.edu.ec>
2024-08-10 06:44:31 -0500

    Configuracion enviar correo

--- backendStore/settings.py
+++ backendStore/settings.py
@@ -165,4 +165,4 @@ EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "storeonlinegye24@gmail.com"
-EMAIL_HOST_PASSWORD = "bnbq tiqf jbha wupy"
+EMAIL_HOST_PASSWORD = "clnu yifi ykef khhi"
