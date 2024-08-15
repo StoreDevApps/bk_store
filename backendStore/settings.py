9854d88ff78fa9abc4b62c14a8337e480e2bbeec
Jorge Aaron Zambrano Choez <joraazam@espol.edu.ec>
2024-08-15 14:50:18 -0500

    BD

--- backendStore/settings.py
+++ backendStore/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
