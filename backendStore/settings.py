060e7b80f9713bc227e0b817b785728bda1b589f
Jorge Aaron Zambrano Choez <joraazam@espol.edu.ec>
2024-08-16 03:22:58 -0500

    modificacion gitignore y trackeo de archivos

--- backendStore/settings.py
+++ backendStore/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config('DATABASE_URL', default='sqlite:///db.sqlite3')
 }
 
 
